39d197f693b483cc12b4e0af5f9459e79bc72a4e
Xalam <ganufvyandana@gmail.com>
2020-11-15 14:37:25 +0700

    Initial commit

---
+++ app/src/main/java/com/xalam/myacademy/data/ContentEntity.kt
@@ -0,0 +1,5 @@
+package com.xalam.myacademy.data
+
+data class ContentEntity (
+    var content: String?
+)
---
+++ app/src/main/java/com/xalam/myacademy/reader/CourseReaderActivity.kt
@@ -0,0 +1,12 @@
+package com.xalam.myacademy.reader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xalam.myacademy.R
+
+class CourseReaderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_reader)
+    }
+}
